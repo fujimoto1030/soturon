4d2ab73515be28d0943d474ab3ce2f5e1a41bd49
藤本　高史 <mac@fujimototakafuminoMacBook-Air.local>
2020-02-19 00:24:06 +0900

    “ファイル更新”

--- git.sh
+++ git.sh
@@ -1,6 +1,3 @@
-LATEST_FILE1="`pwd`/`ls -lt  | head -n 1 | awk '{print $9}'`"
-command1="git add $LATEST_FILE1"
-eval $command1
-
+command1= git add .
 command2= git commit -m “ファイル更新”
 command3= git push origin master
